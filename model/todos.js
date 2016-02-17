73ed38bb576cf69aee6d6c0a29fc4ff10cb532b3
Vladimir Ryabchich <ent-ch@ya.ru>
2016-02-17 18:54:52 +0200

    add ground:db

--- model/todos.js
+++ model/todos.js
@@ -2,5 +2,7 @@
  * Created by netanel on 27/02/15.
  */
 angular.module('todomvc').service('Todos', function() {
-  this.collection = new Meteor.Collection('todos');
+  // this.collection = new Meteor.Collection('todos');
+  this.collection = new Ground.Collection('todos');
+
 });
